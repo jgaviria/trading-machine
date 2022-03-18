287ebace8c9e1e560a6a1c8bbce32ad3252936ac
jgaviria <gaviria08111@gmail.com>
2022-03-18 01:11:39 -0400

    adding naive trader

--- config/config.exs
+++ config/config.exs
@@ -9,10 +9,10 @@
 # move said applications out of the umbrella.
 import Config
 
-# Sample configuration:
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
-#
+config :binance,
+       api_key: "",
+       secret_key: ""
+
+config :logger,
+       level: :info
+
